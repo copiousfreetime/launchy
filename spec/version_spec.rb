f5dce94adadcf5bf56e776d93ac933182cfcb3ea
Jeremy Hinegardner <jeremy@hinegardner.org>
2011-07-17 14:48:14 -0600

    fix test warning

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'Launchy::VERSION' do
   it "should have a #.#.# format" do
-    Launchy::VERSION.must_match /\d+\.\d+\.\d+/
+    Launchy::VERSION.must_match( /\d+\.\d+\.\d+/ )
     Launchy::Version.to_a.each do |n|
       n.to_i.must_be :>=, 0
     end
