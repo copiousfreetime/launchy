d306e3730192cc45c42a6cafd6d7d36b10ef4b35
Jeremy Hinegardner <jeremy@hinegardner.org>
2011-07-16 20:43:58 -0600

    Add in some debug output for finding children

--- lib/launchy/descendant_tracker.rb
+++ lib/launchy/descendant_tracker.rb
@@ -40,8 +40,9 @@ module Launchy
     # and passing all the rest of the parameters to that method in 
     # each child
     def find_child( method, *args )
-      klass = children.find do |klass|
-        klass.send( method, *args )
+      klass = children.find do |child|
+        Launchy.log "Checking if class #{child} is the one for #{method}(#{args.join(', ')})}"
+        child.send( method, *args )
       end
     end
   end
