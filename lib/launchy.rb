d68f19704d67e773c4728e629ba99ec568d86b4b
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2013-11-12 18:24:57 -0700

    revert unneeded change

--- lib/launchy.rb
+++ lib/launchy.rb
@@ -25,7 +25,7 @@ module Launchy
     def open(uri_s, options = {}, &error_block )
       leftover = extract_global_options( options )
       uri = string_to_uri( uri_s )
-      app = app_for_uri_string( uri )
+      app = app_for_uri( uri )
       app.new.open( uri, leftover )
     rescue Launchy::Error => le
       raise le
