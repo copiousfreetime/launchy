2366a3b19ff3eda8e3e4e93e44df8c557e10289b
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2014-11-03 09:52:22 +0800

    Remove GNOME env test
    Testing for Gnome env now also requires testing for the gnome-open
    command.

--- spec/detect/nix_desktop_environment_spec.rb
+++ spec/detect/nix_desktop_environment_spec.rb
@@ -10,19 +10,15 @@ describe Launchy::Detect::NixDesktopEnvironment do
     Launchy.reset_global_options
   end
 
-
-  { "KDE_FULL_SESSION"         => Launchy::Detect::NixDesktopEnvironment::Kde,
-    "GNOME_DESKTOP_SESSION_ID" => Launchy::Detect::NixDesktopEnvironment::Gnome }.each_pair do |k,v|
-    it "can detect the desktop environment of a *nix machine using ENV[#{k}]" do
-      ENV.delete( "KDE_FULL_SESSION" )
-      ENV.delete( "GNOME_DESKTOP_SESSION_ID" )
-      ENV[k] = "launchy-test"
-      nix_env = Launchy::Detect::NixDesktopEnvironment.detect
-      nix_env.must_equal( v )
-      nix_env.browser.must_equal( v.browser )
-      ENV.delete( k )
-    end
-   end
+  it "can detect the desktop environment of a KDE machine using ENV['KDE_FULL_SESSION']" do
+    ENV.delete( "KDE_FULL_SESSION" )
+    ENV["KDE_FULL_SESSION"] = "launchy-test"
+    kde = Launchy::Detect::NixDesktopEnvironment::Kde
+    nix_env = Launchy::Detect::NixDesktopEnvironment.detect
+    nix_env.must_equal( kde )
+    nix_env.browser.must_equal( kde.browser )
+    ENV.delete( 'KDE_FULL_SESSION' )
+  end
 
   it "returns false for XFCE if xprop is not found" do
     Launchy.host_os = "linux"
