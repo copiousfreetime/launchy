9005e164e1c3e95ebb20f57e2490e6d885399df7
copiousfreetime <jeremy@copiousfreetime.org>
2024-02-03 19:22:55 -0700

    attempting some ci caching

---
+++ .semaphore/ensure-ruby-version.sh
@@ -0,0 +1,12 @@
+cache_key="${SEMAPHORE_AGENT_MACHINE_OS_IMAGE}-${RUBY_VERSION}"
+
+if cache has_key "${cache_key}"; then
+  echo "Ruby ${RUBY_VERSION} found in cache"
+  cache restore "${cache_key}"
+  sem-version ruby "${RUBY_VERSION}" -f
+else
+  echo "Installing Ruby $RUBY_VERSION"
+  sem-version ruby "${RUBY_VERSION}" -f
+  cache store "${cache_key}" "${HOME}/.rbenv/versions/${RUBY_VERSION}"
+fi
+
