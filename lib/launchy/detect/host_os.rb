e836549a2537cabc8afecdf31c81f27e566bf655
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2024-05-02 21:39:38 -0600

    removing compact style

--- lib/launchy/detect/host_os.rb
+++ lib/launchy/detect/host_os.rb
@@ -2,32 +2,34 @@
 
 require "rbconfig"
 
-module Launchy::Detect
-  # Internal: Determine the host operating system that Launchy is running on
-  #
-  class HostOs
-    attr_reader :host_os
-    alias to_s host_os
-    alias to_str host_os
+module Launchy
+  module Detect
+    # Internal: Determine the host operating system that Launchy is running on
+    #
+    class HostOs
+      attr_reader :host_os
+      alias to_s host_os
+      alias to_str host_os
 
-    def initialize(host_os = nil)
-      @host_os = host_os
+      def initialize(host_os = nil)
+        @host_os = host_os
 
-      if not @host_os then
-        if (@host_os = override_host_os) then
-          Launchy.log "Using LAUNCHY_HOST_OS override value of '#{Launchy.host_os}'"
-        else
-          @host_os = default_host_os
+        if not @host_os then
+          if (@host_os = override_host_os) then
+            Launchy.log "Using LAUNCHY_HOST_OS override value of '#{Launchy.host_os}'"
+          else
+            @host_os = default_host_os
+          end
         end
       end
-    end
 
-    def default_host_os
-      ::RbConfig::CONFIG["host_os"].downcase
-    end
+      def default_host_os
+        ::RbConfig::CONFIG["host_os"].downcase
+      end
 
-    def override_host_os
-      Launchy.host_os
+      def override_host_os
+        Launchy.host_os
+      end
     end
   end
 end
